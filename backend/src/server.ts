b16cc31609d37ef6f9dbb7cc31bea0435ba8742c
anharathoi <anharzihan@gmail.com>
2021-04-26 16:26:44 +1000

    Added 1st query

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,47 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
+
+// Mock in memory data
+const itemList = [
+  {
+    id: 1,
+    title: "Retro",
+    done: false
+  },
+  {
+    id: 2,
+    title: "Planning",
+    done: false
+  },
+  {
+    id: 3,
+    title: "Ship PR",
+    done: true
+  },
+  {
+    id: 4,
+    title: "Fix code",
+    done: true
+  },
+
+]
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Item {
+    id: Int
+    title: String
+    done: Boolean
+  }
   type Query {
-    hello: String
+    getAllItems: [Item]
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    getAllItems: () => itemList,
   },
 };
 
